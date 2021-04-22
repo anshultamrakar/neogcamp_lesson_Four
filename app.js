6515d2d2b61d11e4358189db7d00b247ef611153
anshultamrakar <anshultamrakar487@gmail.com>
2021-04-22 13:00:09 -0400

    Complete Banana Talk
    Completed

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
-var username = prompt("Give me your Name");
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output")
+console.log(txtInput);
 
-var welcomeMessage = "this script will work" + username;
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-alert(welcomeMessage);
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+
+function clickHandler(){
+    // console.log("clicked");
+    // console.log("input", txtInput.value);
+    // outputDiv.innerText = "ojas" + txtInput.value
+
+    var inputText = txtInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
+    
+   
+
+};
+
+btnTranslate.addEventListener("click",  clickHandler)
+
+
